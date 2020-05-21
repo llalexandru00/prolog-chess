6c04ad79749bb69bd4aad2ccd0abd2687f22e365
Alexandru Lungu <lungualex00@gmail.com>
2020-05-21 13:59:42 +0300

    implemented the bishop and the knight

--- main.pl
+++ main.pl
@@ -74,9 +74,11 @@ move_piece(Turn, Piece, A, B, X, Y, Before, After) :-
   add_piece(Turn, Piece, X, Y, Middle, After)
 .
 
+
 % PREDICATES TO CHECK IF WE CAN MOVE A PAWN TO A SPECIFIC LOCATION
 % THERE ARE 2 TRIES FOR SIMPLE MOVES: ONE TO HANDLE ONE STEP FORWARD AND 
 % ONE TO HANDLE TWO STEPS FORWARD IF IT WASN'T MOVED UNTIL NOW 
+% A DIFFERENCE BETWEEN WHITE AND BLACK SHOULD BE MADE AS MOVES ARE NOT SYMETRICAL
 can_move(white, pawn, C, L, Board, C, FROML, 1) :-
   up(FROML, L),
   empty(Board, C, L),
@@ -101,17 +103,157 @@ can_move(black, pawn, C, 5, Board, C, 7, 2) :-
   exists(black, pawn, Board, C, 7)
 .
 
-% MOVE A BISHOP
-make_move([bishop | _], Before, Before, Turn) :-
-  write(ok),
-  write(Turn)
+
+% PREDICATES TO CHECK IF WE CAN MOVE A KNIGHT TO A SPECIFIC LOCATION
+% THERE ARE 8 DIRECTION FROM WHICH A KNIGHT CAN MOVE
+% WE NEED TO CHECK ONLY IF THE DESTINATION IS EMPTY AS THE KNIGHT AVOIDS COLISION
+% 1 AND 2 ARE NW, 3 AND 4 ARE SW, 5 AND 6 ARE SE, 7 AND 8 ARE NE
+can_move(Turn, knight, C, L, Board, C2, L2, 1) :-
+  up(L, TL),
+  up(TL, L2),
+  up(C2, C),
+  empty(Board, C, L),
+  exists(Turn, knight, Board, C2, L2)
 .
+
+can_move(Turn, knight, C, L, Board, C2, L2, 2) :-
+  up(L, L2),
+  up(TC, C),
+  up(C2, TC),
+  empty(Board, C, L),
+  exists(Turn, knight, Board, C2, L2)
+.
+
+can_move(Turn, knight, C, L, Board, C2, L2, 3) :-
+  up(L2, L),
+  up(TC, C),
+  up(C2, TC),
+  empty(Board, C, L),
+  exists(Turn, knight, Board, C2, L2)
+.
+
+can_move(Turn, knight, C, L, Board, C2, L2, 4) :-
+  up(TL, L),
+  up(L2, TL),
+  up(C2, C),
+  empty(Board, C, L),
+  exists(Turn, knight, Board, C2, L2)
+.
+
+can_move(Turn, knight, C, L, Board, C2, L2, 5) :-
+  up(TL, L),
+  up(L2, TL),
+  up(C, C2),
+  empty(Board, C, L),
+  exists(Turn, knight, Board, C2, L2)
+.
+
+can_move(Turn, knight, C, L, Board, C2, L2, 6) :-
+  up(L2, L),
+  up(C, TC),
+  up(TC, C2),
+  empty(Board, C, L),
+  exists(Turn, knight, Board, C2, L2)
+.
+
+can_move(Turn, knight, C, L, Board, C2, L2, 7) :-
+  up(L, L2),
+  up(C, TC),
+  up(TC, C2),
+  empty(Board, C, L),
+  exists(Turn, knight, Board, C2, L2)
+.
+
+can_move(Turn, knight, C, L, Board, C2, L2, 8) :-
+  up(L, TL),
+  up(TL, L2),
+  up(C, C2),
+  empty(Board, C, L),
+  exists(Turn, knight, Board, C2, L2)
+.
+
+
+% PREDICATES TO CHECK IF WE CAN MOVE A BISHOP TO A SPECIFIC LOCATION
+% THERE ARE 4 DIRECTIONS FROM WHICH A BISHOP CAN COME THUS WE WILL HAVE
+% 8 CASES, DUE TO THE FACT THAT WE HAVE ONE BASIC STEP AND ONE RECURSIVE STEP
+% WE SHOULD MAKE SURE THAT THE PATH BETWEEN SOURCE AND DESTINATION IS CLEAR
+% 1 is NW, 2 is SW, 3 is SE, 4 is NE
+can_move(Turn, bishop, C, L, Board, C2, L2, 1, 1) :-
+  up(C2, C),
+  up(L, L2),
+  empty(Board, C, L),
+  exists(Turn, bishop, Board, C2, L2)
+.
+
+can_move(Turn, bishop, C, L, Board, X, Y, 1, 2) :-
+  up(C2, C),
+  up(L, L2),
+  empty(Board, C, L),
+  can_move(Turn, bishop, C2, L2, Board, X, Y, 1, _)
+.
+
+can_move(Turn, bishop, C, L, Board, C2, L2, 2, 1) :-
+  up(C2, C),
+  up(L2, L),
+  empty(Board, C, L),
+  exists(Turn, bishop, Board, C2, L2)
+.
+
+can_move(Turn, bishop, C, L, Board, X, Y, 2, 2) :-
+  up(C2, C),
+  up(L2, L),
+  empty(Board, C, L),
+  can_move(Turn, bishop, C2, L2, Board, X, Y, 2, _)
+.
+
+can_move(Turn, bishop, C, L, Board, C2, L2, 3, 1) :-
+  up(C, C2),
+  up(L2, L),
+  empty(Board, C, L),
+  exists(Turn, bishop, Board, C2, L2)
+.
+
+can_move(Turn, bishop, C, L, Board, X, Y, 3, 2) :-
+  up(C, C2),
+  up(L2, L),
+  empty(Board, C, L),
+  can_move(Turn, bishop, C2, L2, Board, X, Y, 3, _)
+.
+
+can_move(Turn, bishop, C, L, Board, C2, L2, 4, 1) :-
+  up(C, C2),
+  up(L, L2),
+  empty(Board, C, L),
+  exists(Turn, bishop, Board, C2, L2)
+.
+
+can_move(Turn, bishop, C, L, Board, X, Y, 4, 2) :-
+  up(C, C2),
+  up(L, L2),
+  empty(Board, C, L),
+  can_move(Turn, bishop, C2, L2, Board, X, Y, 4, _)
+.
+
 
 % MOVE A PAWN
 make_move([C, L], Before, After, Turn) :-
   col_trans(C, NC),
   can_move(Turn, pawn, NC, L, Before, X, Y, _),
   move_piece(Turn, pawn, X, Y, NC, L, Before, After)
+.
+
+% MOVE A BISHOP
+make_move([bishop, C, L], Before, After, Turn) :-
+  col_trans(C, NC),
+  can_move(Turn, bishop, NC, L, Before, X, Y, _, _),
+  move_piece(Turn, bishop, X, Y, NC, L, Before, After)
+.
+
+% MOVE A KNIGHT
+make_move([knight, C, L], Before, After, Turn) :-
+  col_trans(C, NC),
+  can_move(Turn, knight, NC, L, Before, X, Y, _),
+  move_piece(Turn, knight, X, Y, NC, L, Before, After)
 .
 
 % ITERATE THE LIST OF MOVES
